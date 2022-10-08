2d6355b1e3b288d3bf10c3eeadb16f66e38e4dfc
[] <slavafomenko0@gmail.com>
2022-10-08 17:43:03 +0300

    finish

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
+import sqlite3
 from config import TOKEN
 import telebot
 from telebot import types
+import random
 
 bot = telebot.TeleBot(token=TOKEN)
 
-
+con = sqlite3.connect('a.db',check_same_thread=False)
+cursor = con.cursor()
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
@@ -15,13 +18,29 @@ def welcome(message):
     markup.add(item1,item2)
     bot.send_message(message.chat.id, "Хочешь расскажу тебе шутку?", reply_markup=markup)
 
+def joke(message):
+    cursor.execute(f'SELECT * FROM a WHERE id = {random.randint(1,32640)}')
+    record = cursor.fetchall()
+    markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
+    item1 = types.KeyboardButton('Смешно')
+    item2 = types.KeyboardButton('Давай еще одну')
+    markup.add(item1,item2)
+    bot.send_message(message.chat.id, text=[i[1] for i in record], reply_markup=markup)
+
+
+@bot.message_handler(content_types='text')
+def answer(message):
+    if message.chat.type == 'private':
+        if message.text == 'Смешно' or message.text == 'Давай еще одну':
+            joke(message)
+        else:
+            bot.send_message(message.chat.id, "Что думаешь, самый умный. Я тебя все равно не понимаю)))")
 
 @bot.callback_query_handler(func = lambda call: True)
 def callback(call):
-    try:
-        if call.message:
-            if call.data == 'joke':
-                bot.send_message(call.message.chat.id, "hahahhahaha")
-    except:
-        ...
+    if call.message:
+        if call.data == 'joke':
+            joke(call.message)
+
+
 bot.infinity_polling(timeout=10, long_polling_timeout=5)
